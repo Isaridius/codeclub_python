cde133d0048cbcac2e30fe4ce49136b6997ab57d
andrewtang <andrew.z.tang@gmail.com>
2024-09-25 15:49:07 -0400

    Coding Club mischivities >:)

---
+++ loops.py
@@ -0,0 +1,23 @@
+# incriment variables
+
+# x = int(0)
+# print(x)
+
+# x = x + 1
+# print(x)
+
+# Code will run while a condition is true.
+# Python:
+
+# i = 0
+# while(i < 5):
+	# print(i)
+	# i = i + 1
+
+#for loops
+# compressing while loops into a for loops
+for i in range(0, 5, 1): #First nubmer is the start, 5 is the end, 1 is the incriments.
+    print(f"Wow! {i}!")
+print("NEW CODE")
+for i in range(3, 12, 2):
+    print(f"Wow! {i}!")
---
+++ tudoradd.py
@@ -0,0 +1,14 @@
+#how many questions
+questions = int(input("How many questions? Resopnd in numbers. " ))
+
+# DMOJ answer
+n = input()
+for i in xrange(n)
+    a, b = map(int, input().split()) #with multiple inputs in the same line, we need to use the split function)
+    print(a + b)
+
+
+for i in range(0, questions, 1):
+    x = int(input())
+    y = int(input())
+    print(x+y)
