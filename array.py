edcef26ebcfef8c03b7ce1c6cbc8d9149bfffa35
Isarid <andrew.z.tang@gmail.com>
2024-10-16 15:16:24 -0400

    Coding club (cccfeb18 is actually classwork)

---
+++ array.py
@@ -0,0 +1,5 @@
+skibidis = ['meow', 'lol', 'skibidi', 'fortnite', 'vbucks']
+
+for i in skibidis:
+    print(i)
+    print("next item...")
---
+++ cidplaylist.py
@@ -0,0 +1,10 @@
+def playlist(playlists):
+    arr = playlist.split()
+        hrs = int(arr[0])
+
+# pseudocode
+# so we have 3 playlists
+# each playlist is an array of 3 items of hour minute and second
+# first we will find out how many playlists tehre are
+# then we figure out how many times we have to run the playlist function
+# this playlist function 
